9df58ab513c50000dfe35c926243011aec83efdc
Tyler11223344 <ttrebuchon@live.com>
2018-01-29 18:14:12 -0500

    Interrupts code

---
+++ kernel/Interrupts/Interrupts.asm
@@ -0,0 +1,11 @@
+[bits 32]
+
+global doCli
+doCli:
+    cli
+    ret
+
+global doSti
+doSti:
+    sti
+    ret
---
+++ kernel/Interrupts/Interrupts_c.c
@@ -0,0 +1,13 @@
+#include "Interrupts.h"
+
+
+
+void cli()
+{
+    doCli();
+}
+
+void sti()
+{
+    doSti();
+}
