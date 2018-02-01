e8f1019cc7d00a3276386a6442aedda999609efc
ttrebuchon <ttrebuchon@live.com>
2018-02-01 08:41:44 -0500

    Add Common.h

---
+++ Common.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDED_COMMON_H
+#define INCLUDED_COMMON_H
+
+#include "kernel/c_cpp.h"
+
+#ifdef __cplusplus
+
+#include <cstdint>
+
+#else
+
+#include <stdint.h>
+
+#endif
+
+typedef unsigned char uchar;
+
+#endif
