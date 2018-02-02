c6faf1d3c4987de5e6a73275d937c22de0a343d4
ttrebuchon <ttrebuchon@live.com>
2018-02-02 18:25:24 -0500

    Add Timer code

---
+++ kernel/Timer_c.c
@@ -0,0 +1,28 @@
+#include "Timer.h"
+#include "Interrupts.h"
+#include "Registers.h"
+#include <drivers/Port_IO.h>
+
+#define DEFAULT_FREQ 1193180
+
+uint32_t ticks = 0;
+
+static void timer_callback(Registers_t regs)
+{
+	++ticks;
+}
+
+void init_timer(uint32_t freq)
+{
+	register_interrupt_handler(IRQ0, &timer_callback);
+	
+	uint32_t div = DEFAULT_FREQ / freq;
+	
+	port_byte_out(0x43, 0x36);
+	
+	uint8_t high = (uint8_t)((div >> 8) & 0xFF);
+	uint8_t low = (uint8_t)(div & 0xFF);
+	
+	port_byte_out(0x40, low);
+	port_byte_out(0x40, high);
+}
