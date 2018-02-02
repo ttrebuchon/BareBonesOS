99b1b7f07fac8977eef4164b85f3ad829031a5fa
ttrebuchon <ttrebuchon@live.com>
2018-02-02 18:22:50 -0500

    Cleanup Port_IO

--- drivers/PortIO.c
+++ drivers/PortIO.c
@@ -1,9 +1,7 @@
 #include "PortIO.h"
 
 
-#ifdef __cplusplus
-extern "C"
-#endif
+
 unsigned char port_byte_in(const unsigned short port)
 {
     unsigned char data;
@@ -11,17 +9,13 @@ unsigned char port_byte_in(const unsigned short port)
     return data;
 }
 
-#ifdef __cplusplus
-extern "C"
-#endif
+
 void port_byte_out(const unsigned short port, const unsigned char data)
 {
     __asm__("out %%al, %%dx" : :"a" (data), "d" (port));
 }
 
-#ifdef __cplusplus
-extern "C"
-#endif
+
 unsigned short port_word_in(const unsigned short port)
 {
     unsigned short data;
@@ -29,11 +23,8 @@ unsigned short port_word_in(const unsigned short port)
     return data;
 }
 
-#ifdef __cplusplus
-extern "C"
-#endif
+
 void port_word_out(const unsigned short port, const unsigned short data)
 {
     __asm__("out %%ax, %%dx" : :"a" (data), "d" (port));
 }
-
--- drivers/PortIO.h
+++ drivers/PortIO.h
@@ -2,25 +2,20 @@
 #define INCLUDED_PORT_IO_H
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
+
+
 unsigned char port_byte_in(const unsigned short port);
 
-#ifdef __cplusplus
-extern "C"
-#endif
 void port_byte_out(const unsigned short port, const unsigned char data);
 
-#ifdef __cplusplus
-extern "C"
-#endif
 unsigned short port_word_in(const unsigned short port);
 
-#ifdef __cplusplus
-extern "C"
-#endif
 void port_word_out(const unsigned short port, const unsigned short data);
 
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif
