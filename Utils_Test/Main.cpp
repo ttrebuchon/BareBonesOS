df738405f2aa0248d55b5b212aefa21bc19c5752
ttrebuchon <ttrebuchon@live.com>
2018-02-06 06:10:48 -0500

    Small Utils_Test work

--- Utils_Test/Main.cpp
+++ Utils_Test/Main.cpp
@@ -1,11 +1,24 @@
 #include "Tests.hh"
+#include <iostream>
 
+#define RUN(X) do { \
+	std::cout << "-----------------\n" << "Running test for " << #X << "...\n-----------------\n" << std::endl; \
+	Test_##X(); \
+	std::cout << "-----------------\n" << #X << " testing done.\n-----------------\n\n\n\n" << "----------------------------------\n\n\n\n\n" << std::endl; \
+	} while (false)
+
+#define RUNC(X) do { \
+	std::cout << "-----------------\n" << "Running C test for " << #X << "...\n-----------------\n" << std::endl; \
+	TestC_##X(); \
+	std::cout << "-----------------\n" << #X << " C testing done.\n-----------------\n\n\n\n" << "----------------------------------\n\n\n\n\n" << std::endl; \
+	} while (false)
 
 int main()
 {
-	Test_List();
-	TestC_Bitset();
-	Test_Bitset();
+	RUN(List);
+	RUNC(Bitset);
+	RUN(Bitset);
+	RUN(IDE);
 	
 	std::cerr << "\n\n\nAll Done!\n" << std::flush;
 }
