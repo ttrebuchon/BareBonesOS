ecff97f7f369becb44dd2439f3df119fa3676357
Tyler11223344 <ttrebuchon@live.com>
2018-02-14 19:48:20 -0500

    InitRD work

--- kernel/Filesystem/initrd.cpp
+++ kernel/Filesystem/initrd.cpp
@@ -1,4 +1,6 @@
 #include "initrd.hh"
+#include "initrd/DirectoryNode.hh"
+#include "initrd/FileNode.hh"
 
 namespace Kernel { namespace Filesystem {
 	
@@ -28,39 +30,46 @@ namespace Kernel { namespace Filesystem {
 	Node* readFile(void* addr, void** endAddr)
 	{
 		unsigned long span = *(unsigned long*)addr;
-		char* recordName = (char*)(addr + sizeof(unsigned long));
+		char* recordName = (char*)addr + sizeof(unsigned long);
 		int i = 0;
 		while (recordName[i++] != 0x0) ;
 		void* data = recordName + i;
-		auto size = span - sizeof(int) - (data - addr);
+		auto size = span - sizeof(int) - ((uint32_t)data - (uint32_t)addr);
 		if (endAddr != nullptr)
 		{
-			*endAddr = data + size;
+			*endAddr = (char*)data + size;
 		}
+
+		auto file = new Init_RD::FileNode(recordName, data, size);
 		
+		return file;
 	}
 	
 	Node* readFolder(void* addr, void** end)
 	{
 		unsigned long span = *(unsigned long*)addr;
-		char* recordName = (char*)(addr + sizeof(unsigned long));
+		char* recordName = (char*)addr + sizeof(unsigned long);
 		int i = 0;
 		while (recordName[i++] != 0x0) ;
 		void* data = recordName + i;
 		unsigned int count = *(unsigned int*)data;
-		void* entries = data + sizeof(unsigned int);
+		void* entries = (char*)data + sizeof(unsigned int);
+
+		auto dir = new Init_RD::DirectoryNode(recordName);
 		
 		void* ptr = entries;
 		void* tmp;
 		for (int j = 0; j < count; ++j)
 		{
 			Node* entry = readRecord(ptr, &tmp);
+			dir->addChild(entry);
 			ptr = tmp;
 		}
 		if (end != nullptr)
 		{
 			*end = ptr;
 		}
+		return dir;
 	}
 }
 }
