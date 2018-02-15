a7245cbba8a6ba01ce48517c83e3afed657be3e2
Tyler11223344 <ttrebuchon@live.com>
2018-02-14 19:46:37 -0500

    PortIO work

--- drivers/PortIO.c
+++ drivers/PortIO.c
@@ -28,3 +28,17 @@ void port_word_out(const unsigned short port, const unsigned short data)
 {
     __asm__("out %%ax, %%dx" : :"a" (data), "d" (port));
 }
+
+void port_long_out(const unsigned short port, const uint32_t data)
+{
+    __asm__("outl %%ax, %%dx" : :"a" (data), "d" (port));
+    //asm volatile ("outl %%ax, %%dx" : : "r"(port), "r"(data));
+}
+
+uint32_t port_long_in(const unsigned short port)
+{
+    uint32_t data;
+    //asm volatile ("inl %%dx, %%ax" : "=r"(data) : "r"(port));
+    __asm__("inl %%dx, %%ax" : "=a" (data) : "d" (port));
+    return data;
+}
--- drivers/PortIO.h
+++ drivers/PortIO.h
@@ -1,6 +1,8 @@
 #ifndef INCLUDED_PORT_IO_H
 #define INCLUDED_PORT_IO_H
 
+#include <Common.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,6 +15,9 @@ void port_byte_out(const unsigned short port, const unsigned char data);
 unsigned short port_word_in(const unsigned short port);
 
 void port_word_out(const unsigned short port, const unsigned short data);
+
+void port_long_out(const unsigned short port, const uint32_t data);
+uint32_t port_long_in(const unsigned short port);
 
 #ifdef __cplusplus
 }
