4405dd7ea2e9195cec2fbc1e36ed245eea621454
Tyler11223344 <ttrebuchon@live.com>
2018-02-14 19:45:47 -0500

    PCI Work

--- drivers/PCI.cpp
+++ drivers/PCI.cpp
@@ -1,13 +1,7 @@
 #include "PCI.hh"
-
+#include "PortIO.h"
 
 namespace Drivers {
-	
-	void outl(unsigned char port, uint32_t data)
-	{
-		asm volatile ("outl %0, %1" : : "r"(port), "r"(data));
-		
-	}
 	
 	uint16_t PCI::ConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
 	{
@@ -19,6 +13,28 @@ namespace Drivers {
 		
 		address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset << 0xfc) | ((uint32_t)0x80000000));
 		
-		outl(0xCF8, address);
+		port_long_out(0xCF8, address);
+
+		tmp = (uint16_t)((port_long_in(0xCFC) >> ((offset & 2) * 8)) & 0xffff);
+		return tmp;
+	}
+
+	uint16_t PCI::CheckVendor(uint8_t bus, uint8_t slot)
+	{
+		uint16_t vendor, device;
+		if ((vendor = ConfigReadWord(bus, slot, 0, 0)) != 0xFFFF)
+		{
+			device = ConfigReadWord(bus, slot, 0, 2);
+		}
+		return vendor;
+	}
+
+
+	bool PCI::CheckDevice(uint8_t bus, uint8_t device)
+	{
+		uint8_t function = 0;
+		uint16_t vendorID;
+
+		//vendorID = CheckVendor(bus, 
 	}
 }
--- drivers/PCI.hh
+++ drivers/PCI.hh
@@ -14,6 +14,8 @@ namespace Drivers {
 		
 		
 		static uint16_t ConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+		static uint16_t CheckVendor(uint8_t bus, uint8_t slot);
+		static bool CheckDevice(uint8_t bus, uint8_t device);
 	};
 	
 }
