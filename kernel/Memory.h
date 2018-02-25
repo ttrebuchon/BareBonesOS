3a8c3c077dd6d4d454beebb5f3ab313e6b84819a
Tyler11223344 <ttrebuchon@live.com>
2018-02-25 16:46:56 -0500

    Improve Memory.h

--- kernel/Memory.h
+++ kernel/Memory.h
@@ -1,1 +1,15 @@
-#include "Memory/kheap.hh"
+
+#ifdef __cplusplus
+    #include "Memory/kheap.hh"
+
+#else
+    void* kmalloc(uint32_t size, int align, uint32_t* phys);
+
+    void kmemset(void*, unsigned char, uint32_t);
+
+    void* malloc(size_t);
+    void free(void*);
+#endif
+
+#include "Memory/memcpy.h"
+
