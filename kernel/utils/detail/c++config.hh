762fcb5840c9b4f2eb08c838538f1227fb5ef688
Tyler11223344 <ttrebuchon@live.com>
2018-02-28 13:19:43 -0500

    Modify _GLIBCXX_THROW_OR_ABORT

--- kernel/utils/detail/c++config.hh
+++ kernel/utils/detail/c++config.hh
@@ -1,4 +1,4 @@
-
+#include <kernel/Error.h>
 
 
 #ifndef _GLIBCXX_SYNCHRONIZATION_HAPPENS_BEFORE
@@ -23,7 +23,11 @@
 
 #else
 
-#define _GLIBCXX_THROW_OR_ABORT(_EXC) (__builtin_abort())
+    #ifndef TESTING
+    #define _GLIBCXX_THROW_OR_ABORT(_EXC) (KPANIC(""))
+    #else
+    #define _GLIBCXX_THROW_OR_ABORT(_EXC) (__builtin_abort())
+    #endif
 
 #endif
 
