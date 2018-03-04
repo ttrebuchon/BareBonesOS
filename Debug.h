b7bf1dceee764af981319a2e10dcc89aade3415d
Tyler11223344 <ttrebuchon@live.com>
2018-03-04 14:04:56 -0500

    Comment out Debug.h

--- Debug.h
+++ Debug.h
@@ -1,37 +1,46 @@
-#ifndef INCLUDED_DEBUG_H
-#define INCLUDED_DEBUG_H
+// #ifndef INCLUDED_DEBUG_H
+// #define INCLUDED_DEBUG_H
 
-//Includes and whatnot
-
-
-
-
-
-#endif
+// //Includes and whatnot
 
 
 
 
-#ifdef DEBUG
 
-#ifndef INCLUDED_DEBUG_H_ENABLED
-#define INCLUDED_DEBUG_H_ENABLED
-#undef INCLUDED_DEBUG_H_DISABLED
-#undef kassert
+// #endif
 
-//TODO: kassert
-#define kassert(X) 
-
-#endif
-#else
-
-#ifndef INCLUDED_DEBUG_H_DISABLED
-#define INCLUDED_DEBUG_H_DISABLED
-#undef INCLUDED_DEBUG_H_ENABLED
-#undef kassert
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
 
 
-#define kassert(X) 
+// #ifdef DEBUG
 
-#endif
-#endif
+// #ifndef INCLUDED_DEBUG_H_ENABLED
+// #define INCLUDED_DEBUG_H_ENABLED
+// #undef INCLUDED_DEBUG_H_DISABLED
+// #undef kassert
+
+// //TODO: kassert
+// #define kassert(X) do {} while (1);
+
+// #endif
+// #else
+
+// #ifndef INCLUDED_DEBUG_H_DISABLED
+// #define INCLUDED_DEBUG_H_DISABLED
+// #undef INCLUDED_DEBUG_H_ENABLED
+// #undef kassert
+
+
+// #define kassert(X) 
+
+// #endif
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+// #endif
+#include "kernel/Debug.h"
+#define kassert(x) ASSERT(x)
