a4deab6b18c39e0affd9a56ca9dcb250c583c130
Tyler11223344 <ttrebuchon@live.com>
2018-03-04 14:04:12 -0500

    IDE Work

--- drivers/IDE/IDE.cpp
+++ drivers/IDE/IDE.cpp
@@ -488,8 +488,17 @@ namespace Drivers { namespace IDE {
 					
 			port_byte_out(command, (uchar)ATACmd::IdentifyPacket);
 			delay();
-			insl(data, (uint32_t*)buf, 256);
-			reserved = 1;
+			
+			return true;
+		}
+
+		insl(data, (uint32_t*)buf, 127);
+		reserved = 1;
+
+		for (int k = 0; k < 40; k += 2)
+		{
+			model[k] = buf[ATAIdentify::Model + k + 1];
+			model[k+1] = buf[ATAIdentify::Model + k];
 		}
 				
 		/*
