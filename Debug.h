18eacdfd788ea7adc53b3607b7be6a23d9189064
ttrebuchon <ttrebuchon@live.com>
2018-03-07 07:10:58 -0500

    Fix Debug.h

---
+++ Debug.h
@@ -0,0 +1,62 @@
+/*
+// #ifndef INCLUDED_DEBUG_H
+// #define INCLUDED_DEBUG_H
+
+// //Includes and whatnot
+
+#ifdef TESTING
+#include <assert.h>
+#endif
+
+
+
+
+// #endif
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+
+// #ifdef DEBUG
+
+// #ifndef INCLUDED_DEBUG_H_ENABLED
+// #define INCLUDED_DEBUG_H_ENABLED
+// #undef INCLUDED_DEBUG_H_DISABLED
+// #undef kassert
+
+// //TODO: kassert
+// #define kassert(X) do {} while (1);
+
+// #endif
+// #else
+
+// #ifndef INCLUDED_DEBUG_H_DISABLED
+// #define INCLUDED_DEBUG_H_DISABLED
+// #undef INCLUDED_DEBUG_H_ENABLED
+// #undef kassert
+
+
+// #define kassert(X) 
+
+// #endif
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+//#endif
+//#else
+
+//#ifndef INCLUDED_DEBUG_H_DISABLED
+//#define INCLUDED_DEBUG_H_DISABLED
+//#undef INCLUDED_DEBUG_H_ENABLED
+//#undef kassert
+
+
+#define kassert(X) 
+
+#endif
+#endif*/
+#include "kernel/Debug.h"
+// #endif
