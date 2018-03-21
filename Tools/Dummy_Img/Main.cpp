ce7318bda8d7701790a6319d4638cd02ada302b8
ttrebuchon <ttrebuchon@live.com>
2018-03-21 02:38:06 -0400

    Add Dummy.img generation tool

---
+++ Tools/Dummy_Img/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+int main(int argc, const char* argv[])
+{
+	std::string filename = "Dummy.img";
+	size_t fsize = 64*1024;
+	unsigned char* buffer;
+	if (argc >= 2)
+	{
+		filename = argv[1];
+	}
+	
+	
+	if (argc >= 3)
+	{
+		fsize = std::atoi(argv[2]);
+	}
+	
+	
+	buffer = new unsigned char[fsize];
+	memset(buffer, 0, fsize);
+	std::ofstream file(filename, std::ios::binary);
+	
+	size_t i = 0;
+	while (i < fsize)
+	{
+		*reinterpret_cast<uint32_t*>(buffer) = 0xDEADBABA;
+		i += 4;
+	}
+	
+	
+	
+	
+	
+	
+	std::copy(buffer, buffer + fsize, std::ostreambuf_iterator<char>(file));
+	file.flush();
+	file.close();
+	std::cout << "Done." << std::endl;
+}
