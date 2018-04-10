c039529db99b2376b2e56dab8ed715331e3ca28c
Tyler11223344 <ttrebuchon@live.com>
2018-04-10 06:02:45 -0400

    Add TODOs for initrd dir node funcs

--- kernel/Filesystem/initrd/DirectoryNode.cpp
+++ kernel/Filesystem/initrd/DirectoryNode.cpp
@@ -13,37 +13,43 @@ namespace Kernel { namespace Filesystem { namespace Init_RD {
 
     uint32_t DirectoryNode::read(uint32_t, uint32_t, uint8_t*)
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
     uint32_t DirectoryNode::write(uint32_t, uint32_t, const uint8_t*)
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
     void DirectoryNode::open()
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
     void DirectoryNode::close()
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
     DirEnt* DirectoryNode::readDir(uint32_t)
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
     Node* DirectoryNode::findDir(const char* name)
     {
-
+        // TODO
+		ASSERT(false);
     }
 
 
