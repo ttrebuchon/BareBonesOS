82102fce3ecde276756e141f1c29555aa419c802
ttrebuchon <ttrebuchon@live.com>
2018-04-14 17:47:16 -0400

    Interrupts_cpp.cpp changes

--- kernel/Interrupts/Interrupts_cpp.cpp
+++ kernel/Interrupts/Interrupts_cpp.cpp
@@ -7,13 +7,13 @@ extern "C" {
 	
 	static volatile Utils::atomic_int cli_count(0);
 	
-	volatile void cli() noexcept
+	volatile void cli()
 	{
 		++cli_count;
 		__DO_CLI__;
 	}
 	
-	volatile void sti() noexcept
+	volatile void sti()
 	{
 		if (cli_count.fetch_sub(1) == 0)
 		{
