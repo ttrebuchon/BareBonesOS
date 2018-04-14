e7af4ee34d696bcb2b56459a7eedc218a0bc379f
ttrebuchon <ttrebuchon@live.com>
2018-04-14 17:42:05 -0400

    Utils work

--- Utils/Allocator.hh
+++ Utils/Allocator.hh
@@ -1,6 +1,8 @@
 #ifndef INCLUDED_ALLOCATOR_HH
 #define INCLUDED_ALLOCATOR_HH
 
+#include <Common.h>
+#include "detail/forward.hh"
 #include "BoolTypes.hh"
 #include "ptrdiff.hh"
 
@@ -23,6 +25,25 @@ namespace Utils
 			typedef Allocator<U> other;
 		};
 	};
+	
+	
+	
+	template <>
+	class Allocator<const void>
+	{
+		public:
+		typedef const void* pointer;
+		typedef const void* const_pointer;
+		typedef const void value_type;
+		
+		template <class U>
+		struct rebind
+		{
+			typedef Allocator<U> other;
+		};
+	};
+	
+	
 	
 	
 	template <class T>
@@ -69,18 +90,18 @@ namespace Utils
 		
 		pointer allocate(size_type n, Allocator<void>::const_pointer hint = 0)
 		{
-			return (pointer)operator new[](sizeof(T)*n);
+			return (pointer)operator new(sizeof(T)*n);
 		}
 		
 		void deallocate(pointer p, size_type n)
 		{
-			operator delete[](p);
+			::operator delete((void*)p);
 		}
 		
 		template <class U, class... Args>
 		void construct(U* p, Args&&... args)
 		{
-			new ((void*)p) U(args...);
+			::new ((void*)p) U(forward<Args>(args)...);
 		}
 		
 		template <class U>
@@ -161,6 +182,69 @@ namespace Utils
 	};
 	
 	
+	
+	
+	
+	
+	template <class T>
+	class Allocator<const T>
+	{
+		public:
+		typedef const T value_type;
+		typedef const T* pointer;
+		typedef const T*		const_pointer;
+		typedef const T& reference;
+		typedef const T& const_reference;
+		typedef decltype(sizeof(0))		  size_type;
+		typedef ptrdiff_t	   difference_type;
+		typedef true_type	   propagate_on_container_move_assignment;
+		typedef true_type	   is_always_equal;
+		
+
+		template <class U>
+		struct rebind
+		{
+			typedef Allocator<U> other;
+		};
+		
+		
+		constexpr Allocator() noexcept {}
+		constexpr Allocator(const Allocator&) noexcept {}
+		template <class U>
+		constexpr Allocator(const Allocator<U>&) noexcept {}
+		
+		~Allocator() = default;
+		
+		
+		
+		
+		pointer address(reference x) const noexcept
+		{
+			return &x;
+		}
+		
+		pointer allocate(size_type n, Allocator<void>::const_pointer hint = 0)
+		{
+			return static_cast<pointer>(::operator new(n*sizeof(T)));
+		}
+		
+		void deallocate(pointer p, size_type n)
+		{
+			::operator delete((void*)p);
+		}
+		
+		template <class U, class... Args>
+		void construct(U* p, Args&&... args)
+		{
+			::new ((void*)p) U(forward<Args>(args)...);
+		}
+		
+		template <class U>
+		void destroy(U* u)
+		{
+			u->~U();
+		}
+	};
 	
 	
 	
