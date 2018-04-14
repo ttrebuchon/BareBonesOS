681d105d9ca4108f091db531a8df301fe64ccc13
ttrebuchon <ttrebuchon@live.com>
2018-04-14 17:44:03 -0400

    CppSupport\new.hh changes

--- kernel/CppSupport/new.hh
+++ kernel/CppSupport/new.hh
@@ -3,12 +3,15 @@
 #define INCLUDED_NEW_HH
 
 
-
+#ifndef __USE_STD_NEW__
 inline void *operator new(size_t, void *p) noexcept
 { return p; }
 
 inline void *operator new[](size_t, void *p) noexcept
 { return p; }
+#else
+#include <new>
+#endif
 
 
 #endif
