71b422c90f17071ef508a56f69faafeade942545
ttrebuchon <ttrebuchon@live.com>
2018-04-25 13:11:05 -0400

    Kernel\Utility.hh work

--- kernel/Utility.hh
+++ kernel/Utility.hh
@@ -53,10 +53,10 @@ namespace Kernel
 
 
 
-// ASM_GET
+// ASM_GET(X, Y) -> Y = X
 #define ASM_GET(X, Y) ASM_MOV(%0, X, : "=r"(Y))
 
-// ASM_SET
+// ASM_SET -> X = Y
 #define ASM_SET(X, Y) ASM_MOV(X, %0, : : "r"(Y))
 
 
@@ -68,8 +68,37 @@ namespace Kernel
 #define ASM_READ_ESP(X) ASM_GET(SP, X)
 #define ASM_SET_ESP(X) ASM_SET(SP, X)
 
-#define ASM_READ_EBP(X) //ASM_GET(BP, X)
-#define ASM_SET_EBP(X) //ASM_SET(BP, X)
+#define ASM_READ_EBP(X) ASM_GET(FP, X)
+#define ASM_SET_EBP(X) ASM_SET(FP, X)
+
+
+#define ASM_RET_REG X30
+
+
+#define __IRET__ ERET
+
+#define __CLI__ __do_aarch64_cli()
+#define __STI__ __do_aarch64_sti()
+
+C_CODE
+__attribute__((__always_inline__))
+static inline void __do_aarch64_cli()
+{
+	asm volatile(" \
+	MRS x1, CPSR; \
+	ORR x1, x1, #0x80/0x40; \
+	MSR cpsr_c, x1");
+}
+
+__attribute__((__always_inline__))
+static inline void __do_aarch64_sti()
+{
+	asm volatile(" \
+	MRS x1, CPSR; \
+	BIC x1, x1, #0x80/0x40; \
+	MSR cpsr_c, x1");
+}
+C_END
 
 
 #else
@@ -80,11 +109,17 @@ namespace Kernel
 #define ASM_READ_EBP(X) ASM_GET(%EBP, X)
 #define ASM_SET_EBP(X) ASM_SET(%EBP, X)
 
+#define __IRET__ IRET
+
+#define __CLI__ asm volatile("cli")
+#define __STI__ asm volatile("sti")
+
+
 #endif
 
 #if !defined(TESTING) && !defined(FREE_TESTING)
-	#define __DO_CLI__ asm volatile("cli")
-	#define __DO_STI__ asm volatile("sti")
+	#define __DO_CLI__ __CLI__
+	#define __DO_STI__ __STI__
 #else
 	#define __DO_CLI__
 	#define __DO_STI__ 
