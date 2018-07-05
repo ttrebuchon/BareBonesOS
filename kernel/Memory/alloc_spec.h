363fca332c8c2a84f2e488260c7426c79ba987c0
ttrebuchon <ttrebuchon@live.com>
2018-07-05 08:22:14 -0400

    Add alloc_spec_t

---
+++ kernel/Memory/alloc_spec.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDED_KERNEL_MEMORY_ALLOC_SPEC_H
+#define INCLUDED_KERNEL_MEMORY_ALLOC_SPEC_H
+
+#include <Common.h>
+
+START_NS(Kernel)
+START_NS(Memory)
+
+C_CODE
+
+typedef struct alloc_spec
+{
+	size_t size;
+	size_t align;
+	
+	#ifdef __cplusplus
+	alloc_spec(const size_t sz, const size_t align) : size(sz), align(align)
+	{}
+	
+	alloc_spec() : alloc_spec(0, 0) {}
+	#endif
+}
+alloc_spec_t;
+
+
+
+C_END
+
+END_NS // Memory
+END_NS // Kernel
+
+
+#endif
