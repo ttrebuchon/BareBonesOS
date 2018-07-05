c99e14aa10ba45a10006f769390544dd31991a85
ttrebuchon <ttrebuchon@live.com>
2018-07-05 08:17:58 -0400

    Add nonnull attribute to relevant memory funcs

--- kernel/Memory/memcpy.h
+++ kernel/Memory/memcpy.h
@@ -7,8 +7,8 @@
 extern "C" {
 #endif
 
-void* kmemcpy(void* dest, const void* src, size_t);
-void* memcpy(void* dest, const void* src, size_t);
+void* kmemcpy(void* dest, const void* src, size_t) __attribute__((__nonnull__(1, 2)));
+void* memcpy(void* dest, const void* src, size_t) __attribute__((__nonnull__(1, 2)));
 
 #ifdef __cplusplus
 }
