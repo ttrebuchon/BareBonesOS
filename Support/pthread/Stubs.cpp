a49445ba56f69033fc3a26f442e7309e2c7a1fab
ttrebuchon <ttrebuchon@live.com>
2018-07-07 14:32:07 -0400

    Commit pthread stubs

--- Support/pthread/Stubs.cpp
+++ Support/pthread/Stubs.cpp
@@ -1,52 +1,56 @@
-// #include <pthread.h>
-// #include <Common.h>
+#include <pthread.h>
+#include <Common.h>
 
-// #ifndef PTHREAD_IGNORE
-// 	#define FAIL assert(NOT_IMPLEMENTED)
-// #else
-// 	#define FAIL TRACE("Called.")
-// #endif
+#ifndef PTHREAD_IGNORE
+	#define FAIL assert(NOT_IMPLEMENTED)
+#else
+	#define FAIL 
+#endif
 
-// extern "C" {
+extern "C" {
+	
+	void* pthread_getspecific(pthread_key_t)
+	{
+		FAIL;
+		return nullptr;
+	}
+	
+	int pthread_setspecific(pthread_key_t, const void*)
+	{
+		FAIL;
+		return -1;
+	}
+	
+	int pthread_mutex_lock(pthread_mutex_t*)
+	{
+		FAIL;
+		return -1;
+	}
+	
+	int pthread_mutex_unlock(pthread_mutex_t*)
+	{
+		FAIL;
+		return -1;
+	}
+	
+	int pthread_cond_signal(pthread_cond_t*)
+	{
+		FAIL;
+		return -1;
+	}
+	
+	int pthread_cond_broadcast(pthread_cond_t*)
+	{
+		FAIL;
+		return -1;
+	}
+	
+	int pthread_cond_wait(pthread_cond_t*, pthread_mutex_t*)
+	{
+		FAIL;
+		return -1;
+	}
 	
 	
-	
-// 	// int pthread_setspecific(pthread_key_t, const void*)
-// 	// {
-// 	// 	FAIL;
-// 	// 	return -1;
-// 	// }
-	
-// 	int pthread_mutex_lock(pthread_mutex_t*)
-// 	{
-// 		FAIL;
-// 		return -1;
-// 	}
-	
-// 	int pthread_mutex_unlock(pthread_mutex_t*)
-// 	{
-// 		FAIL;
-// 		return -1;
-// 	}
-	
-// 	int pthread_cond_signal(pthread_cond_t*)
-// 	{
-// 		FAIL;
-// 		return -1;
-// 	}
-	
-// 	int pthread_cond_broadcast(pthread_cond_t*)
-// 	{
-// 		FAIL;
-// 		return -1;
-// 	}
-	
-// 	int pthread_cond_wait(pthread_cond_t*, pthread_mutex_t*)
-// 	{
-// 		FAIL;
-// 		return -1;
-// 	}
-	
-	
-// 	// int __sF;
-// }
+	int __sF;
+}
