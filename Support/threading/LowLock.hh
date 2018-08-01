dbc07bfab1648719c0a00715437917c1d0494683
ttrebuchon <ttrebuchon@live.com>
2018-08-01 14:07:34 -0400

    Change LowLevelLock yielding

--- Support/threading/LowLock.hh
+++ Support/threading/LowLock.hh
@@ -1,16 +1,21 @@
 #ifndef INCLUDED_SUPPORT_THREADING_LOWLOCK_HH
 #define INCLUDED_SUPPORT_THREADING_LOWLOCK_HH
 
+typedef int int_lock_t;
+
 namespace Support::threading
 {
 
 template <class T>
-static inline void __attribute__((__always_inline__)) low_lock(T& val)
+static inline void __attribute__((__always_inline__)) low_lock(T& val, bool yield = true)
 {
 	T tmp = 0;
 	while (!__atomic_compare_exchange_n(&val, &tmp, 1, false, __ATOMIC_ACQUIRE, __ATOMIC_RELAXED))
 	{
-		sched_yield();
+		if (yield)
+		{
+			sched_yield();
+		}
 		tmp = 0;
 	}
 }
